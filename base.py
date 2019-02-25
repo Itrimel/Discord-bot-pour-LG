f3dc5d5e00757560df2674eb39ffdffaedf25af3
Itrimel <a.a@a.com>
2019-02-25 22:39:42 +0100

    Début

---
+++ base.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 25 19:03:43 2019
+
+@author: tete5
+"""
+
+import discord
+from token_file import TOKEN
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+        print ("Sent "+msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
