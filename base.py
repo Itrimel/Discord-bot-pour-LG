1df734c7699fd2b8331d7b3e34b014b4e1dc0400
Itrimel <a.a@a.com>
2019-02-26 17:08:59 +0100

    Ajout du logging

--- base.py
+++ base.py
@@ -7,6 +7,14 @@ Created on Mon Feb 25 19:03:43 2019
 
 import discord
 from token_file import TOKEN
+import logging
+
+#Mise en place du logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.WARNING)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = discord.Client()
 
